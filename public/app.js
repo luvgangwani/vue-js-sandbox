f5872877cb3a3d3959cb95f133e6c02edf7c8b7d
Luv Gangwani <luvgangwani>
2020-05-07 18:09:24 +1000

    Lesson 5 - Events

--- public/app.js
+++ public/app.js
@@ -1,15 +1,22 @@
 new Vue({
     el: '#root',
     data: {
-        header: 'Vue JS Sandbox',
-        message: 'This is the Vue JS sandbox application.',
-        gitUrl: 'https://github.com/luvgangwani',
-        ageValue: 28,
-        linkedInUrlHtml: '<a href="https://www.linkedin.com/in/luv-gangwani/" target="_blank">LinkedIn Profile</a>'
+       age: 28,
+       xVal: 0,
+       yVal: 0
     },
     methods: {
-        greet: function(time){
-            return `Welcome this ${time}! ${this.message}`
+        add: function(increment) {
+            this.age += increment;
+        },
+
+        subtract: function(decrement) {
+            this.age -= decrement;
+        },
+
+        changeXY: function(e) {
+            this.xVal = e.offsetX;
+            this.yVal = e.offsetY;
         }
     }
 })
