bf2e85091d5b1e5410447d473cbec148817d27b0
Luv Gangwani <luvgangwani>
2020-05-07 17:14:43 +1000

    Lesson 3 - Data and Methods

--- public/app.js
+++ public/app.js
@@ -2,6 +2,11 @@ new Vue({
     el: '#root',
     data: {
         header: 'Vue JS Sandbox',
-        message: 'Hello world'
+        message: 'This is the Vue JS sandbox application.'
+    },
+    methods: {
+        greet: function(time){
+            return `Welcome this ${time}! ${this.message}`
+        }
     }
 })
