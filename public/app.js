fdc2f5539d8a7df3cabfc64e22743a49ff17840f
Luv Gangwani <luvgangwani>
2020-05-07 17:33:09 +1000

    Lesson 4 - Data Binding

--- public/app.js
+++ public/app.js
@@ -2,7 +2,10 @@ new Vue({
     el: '#root',
     data: {
         header: 'Vue JS Sandbox',
-        message: 'This is the Vue JS sandbox application.'
+        message: 'This is the Vue JS sandbox application.',
+        gitUrl: 'https://github.com/luvgangwani',
+        ageValue: 28,
+        linkedInUrlHtml: '<a href="https://www.linkedin.com/in/luv-gangwani/" target="_blank">LinkedIn Profile</a>'
     },
     methods: {
         greet: function(time){
