5179125e70d4c3a76c96c2b63b22b8d24fb7f6dc
Luv Gangwani <luvgangwani>
2020-05-08 19:10:31 +1000

    Lesson 9 - Computed properties

--- public/app.js
+++ public/app.js
@@ -2,12 +2,23 @@ new Vue({
     el: '#root',
     data: {
 
-        firstName: '',
-        lastName: '',
-        age: 0
+        a: 0,
+        b: 0,
+        age: 20
        
     },
     methods: {
-        
+        // if addToA and addToB are methods, both the methods are called if either of the property value changes
+    },
+    computed: {
+        // if addToA and addToB are computed properties, only the mehtod(s) associated to the property whose value is changed is called
+
+        addToA: function () {
+            return this.age + this.a;
+        },
+
+        addToB: function () {
+            return this.age + this.b;
+        }
     }
 });
