882838ff3e20533263990a3b1381efb2829a437b
Luv Gangwani <luvgangwani>
2020-05-09 00:59:26 +1000

    Lesson 16 - Refs

--- public/app.js
+++ public/app.js
@@ -1,51 +1,21 @@
-// any change made to a data property in a component remains local to a component, however if the data property is defined outside the component any changes made to that property would be reflected to the entire application
 
-// const data = {
-//     name: 'Test One'
-// }
-
-Vue.component('greeting', {
-    template: `
-    <p>
-    Greetings, I am a re-usable Vue Component. My name is {{ name }}
-    <br/>
-    <button type="button" v-on:click="changeName()">Change my name</button>
-    </p>
-    `,
-    data: function() {
-        return {
-            name: 'Test One'
-        };
-    },
-    methods: {
-        changeName: function() {
-            this.name = 'Test Two';
-        }
-    }
-})
 
 new Vue({
     el: '#vue-app-one',
     data: {
-
+        favouriteFood: ''
     },
     methods: {
         
+        submitFavouriteFood: function() {
+
+            this.favouriteFood = this.$refs.txtFavFood.value;
+
+            alert(`The div html says ${this.$refs.helloWorld.innerHTML} `);
+        }
+
     },
     computed: {
 
-    }
-});
-
-new Vue({
-    el: '#vue-app-two',
-    data: {
-       
-    },
-    methods: {
-        
-    },
-    computed: {
-        
     }
 });
